e0b77e33491236c82e4eea70a4e44576ffe874f8
phamghien <phamghien@gmail.com>
2023-12-29 13:24:51 -0500

    Style adjustments

--- script.js
+++ script.js
@@ -4,16 +4,23 @@ let playerSelection;
 let computerSelection; 
 let playerChoice; 
 
-var ball = document.querySelector(".ball"); 
-
+const playerScoreNumber = document.getElementById("playerScore"); 
+const computerScoreNumber = document.getElementById("computerScore"); 
 const buttons = document.querySelectorAll('.btn'); 
-
 const output = document.querySelector("#output");
+const ball = document.querySelector(".ball"); 
+const gameOverModal = document.getElementById("gameOverModal"); 
+const restartButton = document.getElementById("restart-button")
+
 output.textContent = "Choose your move:"
 
+
+// Button responses when clicked
 buttons.forEach((button)=>{button.addEventListener('click',()=>{
 
+
     playerChoice = button.id;
+
     if (playerChoice == "rock"){
         playerSelection = "rock"; 
     }
@@ -31,11 +38,13 @@ buttons.forEach((button)=>{button.addEventListener('click',()=>{
 
 })
 
+// Generate random computer choice
 function getComputerChoice() {
     const pick = ["rock", "paper", "scissors"]; 
     return pick[Math.floor(Math.random() * pick.length)]; 
 }
 
+// Play game round and update score
 function playRound(playerSelection, computerSelection){
 
     if(playerSelection == computerSelection) {
@@ -43,18 +52,26 @@ function playRound(playerSelection, computerSelection){
     } else if (playerSelection == "rock" && computerSelection == "scissors") {
         output.textContent = "YOU WIN"; 
         win(); 
+        playerScore++; 
     } else if (playerSelection == "paper" && computerSelection == "rock") {
         output.textContent = "YOU WIN"; 
         win(); 
+        playerScore++; 
     } else if (playerSelection == "scissors" && computerSelection == "paper") {
         output.textContent = "YOU WIN"; 
         win(); 
+        playerScore++; 
     } else {
         output.textContent = "YOU LOSE"; 
         lose(); 
+        computerScore++; 
     }
+
+    playerScoreNumber.textContent = playerScore;
+    computerScoreNumber.textContent = computerScore;
 }
 
+// Ball will go left if lose, ball will go right if win
 function lose(){
     ball.classList.add("lose");
     ball.addEventListener("animationend", onAnimationEnd); 
@@ -70,27 +87,33 @@ function onAnimationEnd() {
     ball.classList.remove("win"); 
 }
 
+// Game is over, restart game
+function isGameOver() {
+    return playerScore === 5 || computerScore === 5
+  }
 
-/*
-function gameScore() {
-    
+  /* 
+function openGameOverBox(){
+    gameOverModal.classList.add("active");
+}
 
-    for(let count = 0; count < 5; count++){
-        const playerSelection = prompt("Choose rock, paper, or scissors").toLowerCase(); 
-        const computerSelection = getComputerChoice(); 
-        console.log(playRound(playerSelection, computerSelection));
+function closeGameOverBox(){
+    gameOverModal.classList.remove("active"); 
+}
 
-        if(playRound(playerSelection, computerSelection) == "YOU WIN!") {
-            playerScore++; 
-        } else {
-            computerScore++; 
-        }
+function gameOverText(){
+    if(playerScore == 5){
+        gameOverText.textContent = "You win!"
     }
-
-    if(playerScore > computerScore) {
-        console.log("WINNER.")
-    } else {
-        console.log("LOSER.")
+    if(computerScore == 5){
+        gameOverText.textContent = "You lose!"
     }
 }
-*/
+
+function restartGame() {
+    gameOverModal.classList.remove("active");
+    playerScore = 0; 
+    computerScore = 0; 
+
+} 
+*/ 
